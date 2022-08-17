3cdfccd89d79b421b5f13770d69b196fca7dd148
Carloss8 <111322335+Carlosss8@users.noreply.github.com>
2022-08-17 15:48:50 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "rlutil.h"
+
+using namespace rlutil;
+using namespace std;
+#include "funciones.h"
+
+
+int main()
+
+
+{
+srand(time(NULL));
+setColor(BLACK);
+setBackgroundColor(MAGENTA);
+cls();
+inicio();
+}
