4288dddcc37882d8fd0f4f7e212227e083c1a143
Carloss8 <111322335+Carlosss8@users.noreply.github.com>
2022-08-17 15:51:05 -0300

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+void menu();
+void tirarDado(int dado[]);
+void jugar(int &pdvj1, int &pdvj2, char sorteo1[], char sorteo2[]);
+void inicio();
+bool volver ();
+void comienzo ();
+void creditos ();
+void carteles ();
